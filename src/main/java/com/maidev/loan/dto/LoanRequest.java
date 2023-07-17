c2711297c6ef7ce2d82ebadc084ae6b937c5dffb
maidev2019 <githubmail2019@gmx.de>
2023-07-18 01:02:45 +0200

    adds dto classes

---
+++ src/main/java/com/maidev/loan/dto/LoanRequest.java
@@ -0,0 +1,18 @@
+package com.maidev.loan.dto;
+
+import com.maidev.loan.model.LoanType;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoanRequest {
+    private Double desiredAmount;
+    private Double anualIncome;
+    private LoanType usedForType;
+}
