9e6f8b21fe456c3ce77a5d8ac55b4639c6a3d346
maidev2019 <githubmail2019@gmx.de>
2023-07-23 16:05:02 +0200

    specifies the association between loan and applicant and address

--- src/main/java/com/maidev/loan/model/Loan.java
+++ src/main/java/com/maidev/loan/model/Loan.java
@@ -18,35 +18,27 @@ import lombok.Setter;
 @Entity
 @Table(name="loan")
 public class Loan {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column
-    private Long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column
+  private Long id;
 
-    @Column
-    private Double desiredAmount;
+  @Column
+  private Double desiredAmount;
 
-    @Column
-    private Double anualIncome;
+  @Column
+  private Double anualIncome;
 
-    @Enumerated(EnumType.STRING)	
-    @Column
-    private LoanType usedForType;
+  @Enumerated(EnumType.STRING)	
+  @Column
+  private LoanType usedForType;
 
-    
+  @ManyToOne
+  @JoinColumn(name = "applicant_id")
+  private Applicant applicant;
 
-    @OneToOne(cascade = CascadeType.ALL)
-    @JoinTable(name = "loan_applicant", 
-      joinColumns = { @JoinColumn(name = "loan_id", referencedColumnName = "id") },
-      inverseJoinColumns = { @JoinColumn(name = "applicant_id", referencedColumnName = "id") })
-    private Applicant applicant;
-
-
-    @OneToOne(cascade = CascadeType.ALL)
-    @JoinTable(name = "loan_address", 
-      joinColumns = { @JoinColumn(name = "loan_id", referencedColumnName = "id") },
-      inverseJoinColumns = { @JoinColumn(name = "address_id", referencedColumnName = "id") })
-    private Address address;
-
+  @ManyToOne
+  @JoinColumn(name = "address_id")
+  private Address address;
 
 }
